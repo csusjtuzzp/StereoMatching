3e8688640cdcae25436ebad0f70142d19ca337c7
csusjtuzzp <zheng_zhen_peng@163.com>
2018-04-27 18:34:10 +0800

    -m

--- opencvsgbm.cpp
+++ opencvsgbm.cpp
@@ -17,6 +17,178 @@ typedef uchar PixType;
 typedef short CostType;
 typedef short DispType;
 
+struct PathDirection
+{
+    int start_c,start_r;
+    int end_c,end_r;
+    int dc,dr;
+    int dx,dy;
+};
+
+void initPaths(int height, int width,vector<PathDirection>& Paths)
+{
+    //8 paths from center pixel based on change in X and Y coordinates
+    const int PATHS_PER_SCAN = 8;
+    for(int i = 0;i < PATHS_PER_SCAN; i++)
+    {
+        Paths.push_back(PathDirection());
+    }
+    for(int i = 0 ; i < PATHS_PER_SCAN; i++)
+    {
+        cout << i <<endl;
+        
+        switch(i)
+        {  
+            /* 方向
+                ------>        
+            */
+            case 0:                      
+            Paths[i].start_c = 0 + 1;
+            Paths[i].start_r = 0 + 1;
+
+            Paths[i].end_c = width - 1 - 1;
+            Paths[i].end_r = height - 1 - 1;
+
+            Paths[i].dc = 1;
+            Paths[i].dr = 0;
+
+            Paths[i].dx = 1;
+            Paths[i].dy = 1;
+            break;
+            /* 方向
+                  >
+                 /
+                /        
+            */
+            case 1:
+            Paths[i].start_c = 0 + 1;
+            Paths[i].start_r = height - 1 - 1;
+
+            Paths[i].end_c = width - 1;
+            Paths[i].end_r = -1 + 1;
+
+            Paths[i].dc = 1;
+            Paths[i].dr = 1;
+
+            Paths[i].dx = 1;
+            Paths[i].dy = -1;
+            break;
+
+             /* 方向 
+                 > 
+                |
+                |
+            */
+            case 2:
+            Paths[i].start_c = 0 + 1;
+            Paths[i].start_r = height - 1 - 1;
+
+            Paths[i].end_c = width - 1;
+            Paths[i].end_r = -1 + 1;
+
+            Paths[i].dc = 0;
+            Paths[i].dr = 1;
+
+            Paths[i].dx = 1;
+            Paths[i].dy = -1;
+            break;
+
+            /* 方向
+               <
+                \
+                 \
+            */
+            case 3:
+            Paths[i].start_c = width - 1 - 1;
+            Paths[i].start_r = width - 1 - 1;
+
+            Paths[i].end_c = -1 + 1;
+            Paths[i].end_r = -1 + 1;
+
+            Paths[i].dc = -1;
+            Paths[i].dr = 1;
+
+            Paths[i].dx = -1;
+            Paths[i].dy = -1;
+            break;
+
+            /* 方向
+               <------
+            */
+            case 4:
+            Paths[i].start_c = width - 1 - 1;
+            Paths[i].start_r = 0 + 1;
+
+            Paths[i].end_c = -1 + 1;
+            Paths[i].end_r = height - 1;
+
+            Paths[i].dc = -1;
+            Paths[i].dr = 0;
+
+            Paths[i].dx = -1;
+            Paths[i].dy = 1;
+            break;
+
+            /* 方向
+                /
+               /
+              <
+            */
+            case 5:
+            Paths[i].start_c = width - 1 - 1;
+            Paths[i].start_r = 0 + 1;
+
+            Paths[i].end_c = -1 + 1;
+            Paths[i].end_r = height - 1;
+
+            Paths[i].dc = -1;
+            Paths[i].dr = -1;
+
+            Paths[i].dx = -1;
+            Paths[i].dy = 1;
+            break;
+
+             /* 方向
+               | 
+               |
+                >
+            */
+            case 6:
+            Paths[i].start_c = width - 1 - 1;
+            Paths[i].start_r = 0 + 1;
+
+            Paths[i].end_c = -1 + 1;
+            Paths[i].end_r = height -1;
+
+            Paths[i].dc = 0;
+            Paths[i].dr = -1;
+
+            Paths[i].dx = -1;
+            Paths[i].dy = 1;
+            break;
+
+            /* 方向
+               \
+                \
+                 >  
+            */
+            case 7:
+            Paths[i].start_c = 0 + 1;
+            Paths[i].start_r = 0 + 1;
+
+            Paths[i].end_c = width - 1;
+            Paths[i].end_r = height - 1;
+
+            Paths[i].dc = 1;
+            Paths[i].dr = -1;
+
+            Paths[i].dx = 1;
+            Paths[i].dy = 1;
+            break;
+        }
+    }
+}
+
 // preSobelFilterCap
 void preSobelFilterCap(Mat& leftImage,Mat& rightImage,uchar *tab,const int OFS)
 {
@@ -105,7 +277,7 @@ void CalculateSADCost(const Mat& leftImage,const Mat& rightImage,const int SADWi
                         sum = sum + abs(leftTemp - rightTemp);
                     }
                 }
-                CBTcost[x][y][d] = sum;
+                CBTcost[x][y][d] = sum/((2 *SADWindows + 1) * (2 * SADWindows + 1));
             } 
             int tempIndex = 0;
             for (int d = 1; d < disparity; d++)
@@ -116,7 +288,7 @@ void CalculateSADCost(const Mat& leftImage,const Mat& rightImage,const int SADWi
                       
                 }
             }
-            SADDisparity.at<uchar>(x,y)=(tempIndex * 4);
+            SADDisparity.at<uchar>(x,y)=(tempIndex * 2);
         }          
     }
     imwrite("SADDisparity.jpg",SADDisparity);
@@ -212,7 +384,7 @@ int GetHammingWeight(unsigned int value)
 } 
 
 void CalculateCensusCost(const vector<unsigned int> &leftCensus,const vector<unsigned int> &rightCensus,Mat& CensusDisparity,
-                        const int disparity,const int CensusWindows,vector<vector<vector<int>>>& Cbuf)
+                        const int disparity,const int CensusWindows,vector<vector<vector<long>>>& Cbuf)
 {
     int CensusPixelSum = (2 * CensusWindows + 1) * (2 * CensusWindows + 1);
     int bitlength = (CensusPixelSum % 32 == 0) ? (CensusPixelSum / 32) : ( CensusPixelSum / 32 + 1 );
@@ -247,14 +419,14 @@ void CalculateCensusCost(const vector<unsigned int> &leftCensus,const vector<uns
                      tempIndex = d;  
                  }
             }
-            CensusDisparity.at<uchar>(i,j)=(tempIndex * 4);
+            CensusDisparity.at<uchar>(i,j)=(tempIndex * 2);
         }
     }
     imwrite("census.jpg",CensusDisparity);
     //imwrite("MatchLevel.jpg",MatchLevelImage); 
 }
 
-void CalculateBTHammingDistance(const vector<vector<vector<int>>>& Cbuf,vector<vector<vector<long>>>& Ccost,const int SADWindows)
+void CalculateBTHammingDistance(const vector<vector<vector<long>>>& Cbuf,vector<vector<vector<long>>>& Ccost,const int SADWindows)
 {  
     int height = Cbuf.size();
     int width = Cbuf[1].size();
@@ -284,7 +456,7 @@ void CalculateBTHammingDistance(const vector<vector<vector<int>>>& Cbuf,vector<v
                         sum = sum  + Cbuf[m][n][d];
                     }
                 }
-                Ccost[x][y][d] = sum;
+                Ccost[x][y][d] = sum/((2 *SADWindows + 1) * (2 * SADWindows + 1));
             }
            
             int tempIndex = 0;
@@ -295,13 +467,13 @@ void CalculateBTHammingDistance(const vector<vector<vector<int>>>& Cbuf,vector<v
                      tempIndex = d;      
                 }
             }
-            CensusBTDisparity.at<uchar>(x,y)=(tempIndex * 4);
+            CensusBTDisparity.at<uchar>(x,y)=(tempIndex * 2);
         }
     }
     imwrite("CensusBTDisparity.jpg",CensusBTDisparity);
 }
 
-void CalculateDymProgCost(const vector<vector<vector<int>>>& Cbuf,const int P1,const int P2)
+void CalculateDymProgCost(const vector<vector<vector<long>>>& Cbuf,const int P1,const int P2)
 {
     const int height = Cbuf.size();
     const int width = Cbuf[1].size();
@@ -321,31 +493,97 @@ void CalculateDymProgCost(const vector<vector<vector<int>>>& Cbuf,const int P1,c
             S[i][j].resize(width);
             for(int k = 0;k < width;k++)
             {
-                S[i][j][k].resize(disparity);
+                S[i][j][k].resize(disparity+2);
+                S[i][j][k][0] = S[i][j][k][disparity+1] = SHRT_MAX;
             }
         }     
-    }    
-    vector<int> minLr(Pathnum);
+    }   
+
+    vector<int> minLr(width); //保存上一次的最小值
 
     vector<int> Delta(Pathnum);
 
     vector<int> L(Pathnum);
     
-    vector<vector<int>> Lr;
-    Lr.resize(Pathnum);
-    for(int i = 0;i < Pathnum;i++)
+    vector<int> Lr;
+    Lr.resize(disparity + 2);
+   
+    vector<int> Lrpre;
+    Lrpre.resize(disparity + 2);
+    
+    vector<PathDirection>Paths;
+    initPaths(height, width,Paths);
+
+    for(int path = 0;path < Paths.size();path++)
     {
-        Lr[i].resize(disparity + 2);
-        Lr[i][0] = Lr[i][disparity+1] = SHRT_MAX;
+        cout << "Path" << path << endl;
+        for(int i = Paths[path].start_r;i < Paths[path].end_r;i = i + Paths[path].dy)
+        {
+            //cout << "i" << i << "width"<< width << endl;
+            for(int j = Paths[path].start_c;j < Paths[path].end_c;j = j + Paths[path].dx)
+            {
+                int tempIndex = 0;
+                int prex = j - Paths[path].dc;
+                int prey = i - Paths[path].dr;
+
+                Lrpre.assign(S[path][prey][prex].begin(),S[path][prey][prex].end());
+
+                int Delta = minLr[prex] + P2;
+                cout << Delta << endl;
+
+                minLr[j] = SHRT_MAX;
+
+                for(int d = 0;d < disparity;d++)
+                {
+                    Lr[d+1] = Cbuf[i][j][d] + std::min((int)Lrpre[d + 1], std::min(Lrpre[d] + P1, std::min(Lrpre[d+2] + P1, Delta))) - Delta;  
+                    minLr[j] = std::min(minLr[j],  Lr[d+1]);
+                }
+                S[path][i][j] = Lr;
+                std::swap(Lr,Lrpre);
+            }
+        }
     }
 
-    vector<vector<int>> Lrpre;
-    Lrpre.resize(Pathnum);
-    for(int i = 0;i < Pathnum;i++)
+    vector<vector<vector<long>>> Sbuf;
+
+    Sbuf.resize(height);
+    for(int i = 0;i < height; ++i)
     {
-        Lrpre[i].resize(disparity + 2);
-        Lrpre[i][0] = Lrpre[i][disparity+1] = SHRT_MAX;
+        Sbuf[i].resize(width);
+        for(int j = 0;j < width;++j)
+        {
+            Sbuf[i][j].resize(disparity);
+        }       
     }
+
+
+    for (int i = 0;i < height;i++)
+    {
+        for(int j = 0;j < width;j++)
+        {
+            for(int d = 0;d < disparity;d++)
+            {
+                for(int path=0;path < Pathnum;path++)
+                {
+                    Sbuf[i][j][d] = Sbuf[i][j][d] + S[path][i][j][d + 1];
+                }
+            }
+
+            int tempIndex = 0;
+
+            for (int d = 1; d < disparity; d++)
+            {
+                if (Sbuf[i][j][d] < Sbuf[i][j][tempIndex]) 
+                {  
+                     tempIndex = d;      
+                }
+            }
+
+            CalculateDym.at<uchar>(i,j)=(tempIndex * 2);       
+        }
+    }
+
+    /*
 
     for(int i = 0;i < height;i++)
     {
@@ -355,10 +593,7 @@ void CalculateDymProgCost(const vector<vector<vector<int>>>& Cbuf,const int P1,c
             for(int k = 0;k < 1;k++)
             {
                 Delta[k] = minLr[k] + P2;
-
                 minLr[k] = SHRT_MAX;
-
-               
 
                 for(int d = 0;d < disparity;d++)
                 {
@@ -370,12 +605,14 @@ void CalculateDymProgCost(const vector<vector<vector<int>>>& Cbuf,const int P1,c
                         cout << tempIndex << endl;
                     }     
                 }
+
                 std::swap(Lr[k],Lrpre[k]);
             }          
             CalculateDym.at<uchar>(i,j)=(tempIndex * 5);
         }
     }
-    imwrite("CalculateDym.jpg",CalculateDym);
+    */
+   imwrite("CalculateDym.jpg",CalculateDym);
 
 }
 
@@ -402,9 +639,9 @@ int main()
 
     preSobelFilterCap(leftImage,rightImage,tab,OFS);
 
-    const int disparity = 32;
+    const int disparity = 128;
     const int D = disparity;
-    const int CensusWindows = 9;
+    const int CensusWindows = 5;
     const int SADWindows2 = 5;
 
     vector<unsigned int>leftCensus;
@@ -415,31 +652,38 @@ int main()
 
     CensusCalculate(leftImage,rightImage,CensusWindows,leftCensus,rightCensus,disparity);
 
-    vector<vector<vector<int>>> Cbuf;
+    vector<vector<vector<long>>> Cbuf;
     vector<vector<vector<long>>> CBTcost;
+    vector<vector<vector<long>>> CensusBTcost;
+
     Cbuf.resize(height);
     CBTcost.resize(height);
+    CensusBTcost.resize(height);
     for(int i = 0;i < height; ++i)
     {
         Cbuf[i].resize(width);
         CBTcost[i].resize(width);
+        CensusBTcost[i].resize(width);
+
         for(int j = 0;j < width;++j)
         {
             Cbuf[i][j].resize(disparity);
             CBTcost[i][j].resize(disparity);
+           CensusBTcost[i][j].resize(disparity);
         }       
     }
-
+    
+    CalculateSADCost(leftImage,rightImage,SADWindows2,disparity,CBTcost);
     CalculateCensusCost(leftCensus,rightCensus,CensusDisparity,disparity,CensusWindows,Cbuf);
 
-    // CalculateBTHammingDistance(Cbuf,CBTcost,SADWindows2);
+    CalculateBTHammingDistance(Cbuf,CensusBTcost,SADWindows2);
 
-    //CalculateSADCost(leftImage,rightImage,SADWindows2,disparity,CBTcost);
 
     const int P1 = 3;
     const int P2 = 20;
 
-    CalculateDymProgCost(Cbuf,P1,P2);
+    //CalculateDymProgCost(Cbuf,P1,P2);
+    CalculateDymProgCost(CensusBTcost,P1,P2);
 
     cout << "-----" << endl;
     imshow("left",leftImage);
